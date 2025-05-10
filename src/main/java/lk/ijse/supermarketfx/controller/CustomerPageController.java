8ba61422310e079d68f8e768eeb351c2796de3d9
shamodhas <shamodha7@gmail.com>
2025-05-10 10:38:07 +0530

    initial commit

--- src/main/java/lk/ijse/supermarketfx/controller/CustomerPageController.java
+++ src/main/java/lk/ijse/supermarketfx/controller/CustomerPageController.java
@@ -13,7 +13,9 @@ import lk.ijse.supermarketfx.model.CustomerModel;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.regex.Pattern;
 
 /**
  * --------------------------------------------
@@ -37,11 +39,17 @@ public class CustomerPageController implements Initializable {
 
     // TM - table model Ex: CustomerTM
     public TableView<CustomerTM> tblCustomer;
+
     public TableColumn<CustomerTM, String> colId;
     public TableColumn<CustomerTM, String> colName;
     public TableColumn<CustomerTM, String> colNic;
     public TableColumn<CustomerTM, String> colEmail;
     public TableColumn<CustomerTM, String> colPhone;
+
+    private final String namePattern = "^[A-Za-z ]+$";
+    private final String nicPattern = "^[0-9]{9}[vVxX]||[0-9]{12}$";
+    private final String emailPattern = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
+    private final String phonePattern = "^(\\d+)||((\\d+\\.)(\\d){2})$";
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -66,20 +74,46 @@ public class CustomerPageController implements Initializable {
 
     private void loadTableData() throws SQLException {
         // 1. Long code
-        ArrayList<CustomerDTO> customerDTOArrayList = customerModel.getAllCustomer();
-        ObservableList<CustomerTM> list = FXCollections.observableArrayList();
 
-        for (CustomerDTO customerDTO : customerDTOArrayList){
-            CustomerTM customerTM = new CustomerTM(
-                    customerDTO.getCustomerId(),
-                    customerDTO.getName(),
-                    customerDTO.getNic(),
-                    customerDTO.getEmail(),
-                    customerDTO.getPhone()
-            );
-            list.add(customerTM);
-        }
-        tblCustomer.setItems(list);
+        // Fetch data from DB
+//        ArrayList<CustomerDTO> customerDTOArrayList = customerModel.getAllCustomer();
+//
+//        ObservableList<CustomerTM> list = FXCollections.observableArrayList();
+//        for (CustomerDTO customerDTO : customerDTOArrayList){
+//            CustomerTM customerTM = new CustomerTM(
+//                    customerDTO.getCustomerId(),
+//                    customerDTO.getName(),
+//                    customerDTO.getNic(),
+//                    customerDTO.getEmail(),
+//                    customerDTO.getPhone()
+//            );
+//
+//            list.add(customerTM);
+//        }
+//        tblCustomer.setItems(list);
+
+
+        // 2. Short code
+
+        // dto list -> tm list
+//        List<CustomerTM> list = customerModel.getAllCustomer().stream().map(
+//                customerDTO -> new CustomerTM(
+//                        customerDTO.getCustomerId(),
+//
+//                        )
+//        ).toList();
+
+        tblCustomer.setItems(FXCollections.observableArrayList(
+                customerModel.getAllCustomer().stream().map(customerDTO ->
+                        new CustomerTM(
+                                customerDTO.getCustomerId(),
+                                customerDTO.getName(),
+                                customerDTO.getNic(),
+                                customerDTO.getEmail(),
+                                customerDTO.getPhone()
+                        )).toList()
+        ));
+
     }
 
     private void loadNextId() throws SQLException {
@@ -94,6 +128,18 @@ public class CustomerPageController implements Initializable {
         String phone = txtPhone.getText();
         String customerId = lblId.getText();
 
+        // Shamodha sahan
+//        String namePattern = "^[A-Za-z ]+$";
+        // 1. Using Pattern object java.util.regex
+//        Pattern compiledNamePattern = Pattern.compile(namePattern);
+//        boolean isValidName = compiledNamePattern.matcher(name).matches();
+//        System.out.println(name + " is valid:(1) " + isValidName);
+
+        // 2. Using String class matches() method
+//        boolean matches = name.matches(namePattern);
+//        System.out.println(name + " is valid:(2) " + matches);
+
+
         // DTO - Data transfer object
         // Create dto object wrap data to single unit
         CustomerDTO customerDTO = new CustomerDTO(
@@ -103,36 +149,37 @@ public class CustomerPageController implements Initializable {
                 email,
                 phone
         );
+
         // setter method customerDTO.setNic("");
 
         // Call CustomerModel inside saveCustomer method
         // controller to model parse data using dto
 //        CustomerModel customerModel = new CustomerModel();
-        try {
-            boolean isSave = customerModel.saveCustomer(customerDTO);
-            if (isSave) {
-                loadNextId();
-                loadTableData();
-
-                txtName.setText("");
-                txtNic.setText("");
-                txtEmail.setText("");
-                txtPhone.setText("");
-
-                new Alert(
-                        Alert.AlertType.INFORMATION, "Customer saved successfully..!"
-                ).show();
-            } else {
-                new Alert(
-                        Alert.AlertType.ERROR, "Fail to save customer..!"
-                ).show();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            new Alert(
-                    Alert.AlertType.ERROR, "Fail to save customer..!"
-            ).show();
-        }
+//        try {
+//            boolean isSave = customerModel.saveCustomer(customerDTO);
+//            if (isSave) {
+//                loadNextId();
+//                loadTableData();
+//
+//                txtName.setText("");
+//                txtNic.setText("");
+//                txtEmail.setText("");
+//                txtPhone.setText("");
+//
+//                new Alert(
+//                        Alert.AlertType.INFORMATION, "Customer saved successfully..!"
+//                ).show();
+//            } else {
+//                new Alert(
+//                        Alert.AlertType.ERROR, "Fail to save customer..!"
+//                ).show();
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            new Alert(
+//                    Alert.AlertType.ERROR, "Fail to save customer..!"
+//            ).show();
+//        }
 //       Static method call using classname CustomerModel.saveCustomer();
     }
 
